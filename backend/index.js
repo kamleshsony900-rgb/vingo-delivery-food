dc30ea1a78b6f2df65d3a2a61c0d24d3d6390167
kamleshsony97500-wq <kamleshsony97500@gmail.com>
2026-04-22 19:53:32 +0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -39,6 +39,7 @@ app.use((req, res, next) => {
 
 
 // konse local sever connect kar sakte hai 
+app.set("trust proxy", 1);
 app.use(cors({
   origin: "https://vingo-abwe.onrender.com",
   credentials: true,
