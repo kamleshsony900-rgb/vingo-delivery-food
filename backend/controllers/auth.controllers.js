22b0a2df91d4fa0764b38f6f10fd6ecdb1309996
kamleshsony97500-wq <kamleshsony97500@gmail.com>
2026-04-22 19:51:35 +0400

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -111,7 +111,11 @@ export const signIn = async (req, res) => {
 
 export const signOut = async (req, res) => {
      try {
-        res.clearCookie("token");
+       res.clearCookie("token", {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+});
 
         return res.status(200).json({
             message:"log out successfully!"
